133e3eced728c0796c302a4b19e0c45b1fd148f4
Salvador Cuamatzi <escuamatzi@gmail.com>
2023-07-11 17:30:39 -0600

    now ec2-create.py can create many servers, all of them with unique names. The response for each server is stored in a file by now.

--- ec2-create.py
+++ ec2-create.py
@@ -6,32 +6,38 @@ ec2_client= boto3.client("ec2",region_name="us-east-2")
 #aws_access_key_id
 #aws_secret_access_key
 
-response = ec2_client.run_instances(
-    ImageId="ami-024e6efaf93d85776",
-    InstanceType="t2.micro",
-    KeyName="testkey",
-    MaxCount=1,
-    MinCount=1,
-    Placement={
-        "AvailabilityZone":"us-east-2a"
-    },
-    SecurityGroupIds=["sg-061efb035d2d3124d"],
-    SubnetId="subnet-007b29710ad02f48b",
-    TagSpecifications=[
-        {
-            "ResourceType":"instance",
-            "Tags":[
-                {
-                    "Key":"Name",
-                    "Value":"deploysrv2"
-                },
-            ]
-        },
-    ]
-)
+number_of_servers=3
+item=1
 
-f=open("newservers.txt", "a")
-f.write(str(response)+"\n\n")
-f.close
+while item<=number_of_servers:
+
+    response = ec2_client.run_instances(
+        ImageId="ami-024e6efaf93d85776",
+        InstanceType="t2.micro",
+        KeyName="testkey",
+        MaxCount=1,
+        MinCount=1,
+        Placement={
+            "AvailabilityZone":"us-east-2a"
+        },
+        SecurityGroupIds=["sg-061efb035d2d3124d"],
+        SubnetId="subnet-007b29710ad02f48b",
+        TagSpecifications=[
+            {
+                "ResourceType":"instance",
+                "Tags":[
+                    {
+                        "Key":"Name",
+                        "Value":f"deployserver{item}"
+                    },
+                ]
+            },
+        ]
+    )
+
+    f=open("newservers.txt", "a")
+    f.write(str(response)+"\n\n")
+    f.close
+    item+=1
 
 #pprint.pprint(response)
