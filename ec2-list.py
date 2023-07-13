70238d3255b86a320bc6bfc5160d20e01ebf7756
Salvador Cuamatzi <escuamatzi@gmail.com>
2023-07-13 11:57:46 -0600

    Now ec2-list.py shows the instances names and the IPs of each one. Also a message if there is no instances.

--- ec2-list.py
+++ ec2-list.py
@@ -1,13 +1,24 @@
 import boto3
 import pprint
 
-ec2_client = boto3.client("ec2", region_name="us-east-2")
+ec2_client = boto3.client("ec2", region_name="us-east-1")
 
-#response = client.describe_instances()
 response = ec2_client.describe_instances()
 
-#print(response)
-pprint.pprint(response)
+#pprint.pprint(response) # Print dictionary easy to read
 
-for item in response["Reservations"]:
-    print(len(item["Instances"]))
+if(response["Reservations"]):
+    for item in response["Reservations"]:
+        for instance in item["Instances"]:
+            # Print instance Name:
+            for tag in instance["Tags"]:
+                if tag["Key"]=="Name":
+                    print("Instance: "+tag["Value"])
+            
+            # Print Private IP address
+            print("Private IP:" + instance["PrivateIpAddress"])
+
+            # Print Public IP
+            print("Public IP: "+ instance["PublicIpAddress"] + "\n" )
+else:
+    print("There are no EC2 instances.")
