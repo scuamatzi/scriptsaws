c7aab7467f15b893ef7206ec2ce84adc6d9720ab
Salvador Cuamatzi <escuamatzi@gmail.com>
2023-08-15 16:04:49 -0600

    Just add 2 files in .gitignore.  Also worked on ec2-create-snapshot.py-> it gets info for every ec2 instance on AWS and ask if it should create an snapshot. Now its time to create the snapshot and asign a meaningful description.

--- ec2-create-snapshot.py
+++ ec2-create-snapshot.py
@@ -1,25 +1,52 @@
 import boto3
 import pprint
+from datetime import datetime
 
 ec2_client=boto3.client("ec2",region_name="us-east-2")
 
-response=ec2_client.create_snapshots(
-    Description="Snapshot v4 of Instace EC2",
-    InstanceSpecification={
-        "InstanceId":"i-0c0079030f4cf77d1"
-    },
-    TagSpecifications=[
-        {
-            "ResourceType":"snapshot",
-            "Tags":[
-                {
-                    "Key":"Name",
-                    "Value":"This is a snapshot of a EC2"
-                },
-            ]
-        },
-    ],
-    # DryRun=True,
-)
+print("Script to create snapshots of ec2 instances on AWS")
 
-pprint.pprint(response)
+response_instance=ec2_client.describe_instances()
+
+if(response_instance["Reservations"]):
+    for item in response_instance["Reservations"]:
+        print("There are "+ str(len(item)-1) +" instances")
+        for instance in item["Instances"]:
+            for tag in instance["Tags"]:
+                if tag["Key"]=="Name":
+                    instance_name=tag["Value"]
+                    #print("Instance Name:" + tag["Value"])
+            print(f"Instance Name: {instance_name}")
+            answer=input("Create snapshot? (yes/no): ")
+            if answer.lower()=="yes":
+                print("Creating snapshot")
+                creation_date=datetime.now().strftime("%Y%m%d-%H-%M-%S")
+                print(instance["InstanceId"])
+
+                # response_snapshot=ec2_client.create_snapshots(
+                #     Description=tag["Value"]+" snapshot "+creation_date,
+                #     InstanceSpecification={
+                #         "InstanceId":"i-1c0079030f4cf77d1"
+                #     },
+                #     TagSpecifications=[
+                #         {
+                #             "ResourceType":"snapshot",
+                #             "Tags":[
+                #                 {
+                #                     "Key":"Name",
+                #                     "Value":"This is a snapshot of a EC1"
+                #                 },
+                #             ]
+                #         },
+                #     ],
+                #     # DryRun=True,
+                # )
+            else:
+                print("Skip.....")
+        print("")
+else:
+    print("There are no ec2 instances.")
+
+
+
+#pprint.pprint(response)
