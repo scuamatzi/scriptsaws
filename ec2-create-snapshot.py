144a893874a1043fefdbccf6f41e975f5321392c
Salvador Cuamatzi <escuamatzi@gmail.com>
2023-08-15 17:12:15 -0600

    Now it has comments for future revisions. I just find that it also consider ec2 instances in 'terminated' status, which is a problem.  Next step is to ignore those ec2 instances.

--- ec2-create-snapshot.py
+++ ec2-create-snapshot.py
@@ -6,16 +6,23 @@ ec2_client=boto3.client("ec2",region_name="us-east-2")
 
 print("Script to create snapshots of ec2 instances on AWS\n")
 
+## To get the information of each ec2 instance
 response_instance=ec2_client.describe_instances()
 
+## Verify if there are ec2 instances
 if(response_instance["Reservations"]):
     print("There are "+ str(len(response_instance["Reservations"])) +" instances")
+
     for item in response_instance["Reservations"]:
         for instance in item["Instances"]:
+            ## Get name of each instance
             for tag in instance["Tags"]:
                 if tag["Key"]=="Name":
                     instance_name=tag["Value"]
+
             print(f"Instance Name: {instance_name}")
+
+            ## Decide wether to create a snapshot or not
             answer=input("Create snapshot? (yes/no): ")
             if answer.lower()=="yes":
                 print("Creating snapshot")
@@ -40,6 +47,7 @@ if(response_instance["Reservations"]):
                 )
                 print("Snapshot ID: "+response_snapshot["Snapshots"][0]["SnapshotId"])
                 print("....Done\n")
+            ## If the answer isn't "yes" then skip snapshot creation
             else:
                 print("Skip.....\n")
 else:
